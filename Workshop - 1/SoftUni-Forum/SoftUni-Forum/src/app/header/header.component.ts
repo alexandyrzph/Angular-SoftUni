fe1bf0f73977bda626a51089d5cd1a9176d7b0c0
alexandyrzph <alexandyrzph@gmail.com>
2022-03-14 00:32:04 +0200

    Mocked Login Logout

--- Workshop - 1/SoftUni-Forum/SoftUni-Forum/src/app/header/header.component.ts
+++ Workshop - 1/SoftUni-Forum/SoftUni-Forum/src/app/header/header.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+    get isLogged(): boolean {
+        return this.userService.isLogged;
+    }
 
-  ngOnInit(): void {
-  }
+    constructor(
+        public userService: UserService
+    ) { }
+
+    loginHandler(): void {
+        this.userService.login();
+    }
+
+    logoutHandler(): void {
+        this.userService.logout();
+    }
 
 }
--- Workshop - 1/SoftUni-Forum/SoftUni-Forum/src/app/user.service.ts
+++ Workshop - 1/SoftUni-Forum/SoftUni-Forum/src/app/user.service.ts
@@ -5,5 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
 
+    isLogged = false;
     constructor() { }
+
+    login(): void {
+        this.isLogged = true;
+    }
+
+    logout(): void {
+        this.isLogged = false;
+    }
+
 }
