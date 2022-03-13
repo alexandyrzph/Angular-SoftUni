4b2ce991ca7ce1028c65a9161b4fad0347a424b4
alexandyrzph <alexandyrzph@gmail.com>
2022-03-13 22:43:44 +0200

    Configured styles

--- Workshop - 1/SoftUni-Forum/SoftUni-Forum/src/app/app.module.ts
+++ Workshop - 1/SoftUni-Forum/SoftUni-Forum/src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ThemeListComponent } from './theme-list/theme-list.component';
+import { AsideComponent } from './aside/aside.component';
+import { ThemeListItemComponent } from './theme-list-item/theme-list-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        ThemeListComponent,
+        AsideComponent,
+        ThemeListItemComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent
+    ]
 })
 export class AppModule { }
